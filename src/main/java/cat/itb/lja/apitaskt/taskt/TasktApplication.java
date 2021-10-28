e60deb00402f76051fc7f377bac39ffd77085ff8
sjo <asd@as.com>
2021-10-28 16:20:23 +0200

    inicial

---
+++ src/main/java/cat/itb/lja/apitaskt/taskt/TasktApplication.java
@@ -0,0 +1,13 @@
+package cat.itb.lja.apitaskt.taskt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasktApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/itb/lja/apitaskt/taskt/TasktApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itb.lja.apitaskt.taskt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
