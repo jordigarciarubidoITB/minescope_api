558ce14b3f09e7522ee756ee89407e85ddb16da1
osihfdshaj <soijadgjao>
2021-10-29 15:40:16 +0200

    added controller

--- src/main/java/cat/itb/lja/apitaskt/taskt/controladors/ControladorTaskt.java
+++ src/main/java/cat/itb/lja/apitaskt/taskt/controladors/ControladorTaskt.java
@@ -1,5 +1,41 @@
 package cat.itb.lja.apitaskt.taskt.controladors;
 
-public class ControladorTaskt {
+import cat.itb.lja.apitaskt.taskt.model.entitats.Item;
+import cat.itb.lja.apitaskt.taskt.model.serveis.ServeiItem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ControladorTaskt {
+    private final ServeiItem serveiItem;
+
+    @GetMapping("/items")
+    public List<Item> llistarItems() {
+        return serveiItem.llistarItems();
+    }
+
+    @GetMapping("/items/{id}")
+    public Item consultarItem(@PathVariable int id)
+    {
+        return serveiItem.consultarItem(id);
+    }
+
+    @PostMapping("/items")
+    public Item crearItem(@RequestBody Item nou){
+        return serveiItem.afegirItem(nou);
+    }
+
+    @DeleteMapping("/items/{id}")
+    public Item eliminarItem(@PathVariable int id){
+        return serveiItem.eliminarItem(id);
+    }
+
+    //per modificar un usuari existent
+    @PutMapping("/items")
+    public Item modificarItem(@RequestBody Item mod){
+        return serveiItem.modificarItem(mod);
+    }
 }
