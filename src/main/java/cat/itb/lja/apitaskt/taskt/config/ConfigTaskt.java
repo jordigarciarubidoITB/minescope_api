24e5e5fd8df96cfac8ed23ebe7d3368e41ad3a39
jordi <jordi.garcia.7e4@itb.cat>
2021-11-12 15:54:59 +0100

    Config

--- src/main/java/cat/itb/lja/apitaskt/taskt/config/ConfigTaskt.java
+++ src/main/java/cat/itb/lja/apitaskt/taskt/config/ConfigTaskt.java
@@ -1,9 +1,11 @@
 package cat.itb.lja.apitaskt.taskt.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class ConfigTaskt {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
