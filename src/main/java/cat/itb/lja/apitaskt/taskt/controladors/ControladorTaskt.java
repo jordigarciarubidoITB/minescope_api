129d0947d267347cb83fcdde6d273395a3b658fd
jnafjn <okdfkndfskng>
2022-01-10 16:47:52 +0100

    NewEndpoints

--- src/main/java/cat/itb/lja/apitaskt/taskt/controladors/ControladorTaskt.java
+++ src/main/java/cat/itb/lja/apitaskt/taskt/controladors/ControladorTaskt.java
@@ -18,107 +18,6 @@ public class ControladorTaskt {
     private final ServeiLlista serveiLlista;
     private final ServeiUsuari serveiUsuari;
 
-    //LLISTES
-
-    @GetMapping("/todolists")
-    public ResponseEntity<?> llistarLlistes() {
-        if (serveiLlista.llistarLlistes() == null) return ResponseEntity.notFound().build();
-        else return ResponseEntity.ok(serveiLlista.llistarLlistes());
-    }
-
-    @PostMapping("/todolists")
-    public ResponseEntity<?> crearLlista(@RequestBody Llista nou){
-        Llista res = serveiLlista.afegirLlista(nou);
-        return new ResponseEntity<Llista>(res, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/todolists/{idLlista}")
-    public ResponseEntity<?> consultarLlista(@PathVariable int idLlista) {
-        Llista res = serveiLlista.consultarLlista(idLlista);
-        if (res == null) return ResponseEntity.notFound().build();
-        else return ResponseEntity.ok(res);
-    }
-
-    @DeleteMapping("/todolists/{idLlista}")
-    public ResponseEntity<?> eliminarLlista(@PathVariable int idLlista) {
-        Llista res = serveiLlista.eliminarLlista(idLlista);
-        if (res == null) return ResponseEntity.notFound().build();
-        else return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/todolists")
-    public ResponseEntity<?> modificarLlista(@RequestBody Llista mod) {
-        Llista res = serveiLlista.modificarLlista(mod);
-        if (res == null) return ResponseEntity.notFound().build();
-        else return ResponseEntity.ok(res);
-    }
-
-    @GetMapping("/todolists/{idLlista}/todoitems")
-    public ResponseEntity<?> llistarItemsLlista(@PathVariable int idLlista) {
-        Llista res = serveiLlista.consultarLlista(idLlista);
-        if (res == null) return ResponseEntity.notFound().build();
-        else{
-            return ResponseEntity.ok(res.getLlistaItem());
-        }
-    }
-
-    @GetMapping("/todolists/{idLlista}/todoitems/{idItem}")
-    public ResponseEntity<?> consultarItemLlista(@PathVariable int idLlista, @PathVariable int idItem) {
-        Llista res = serveiLlista.consultarLlista(idLlista);
-        Item res2 = serveiItem.consultarItem(idItem);
-        if (res == null) return ResponseEntity.notFound().build();
-        else{
-            if (res2 == null) return ResponseEntity.notFound().build();
-            else return ResponseEntity.ok(res2);
-        }
-    }
-
-    //si es pot crear es retorna CREATED
-    @PostMapping("/todolists/{idLlista}/todoitems")
-    public ResponseEntity<?> crearItem(@PathVariable int idLlista, @RequestBody Item nou){
-        Llista res = serveiLlista.consultarLlista(idLlista);
-        if (res == null) return ResponseEntity.notFound().build();
-        else {
-            if (nou.getIdLlista() != idLlista) return ResponseEntity.notFound().build();
-            else {
-                Item res2 = serveiItem.afegirItem(nou);
-                return new ResponseEntity<Item>(res2, HttpStatus.CREATED);
-            }
-        }
-    }
-
-    @PutMapping("/todolists/{idLlista}/todoitems/{idItem}")
-    public ResponseEntity<?> modificarItem(@PathVariable int idLlista,@PathVariable int idItem) {
-        Llista res = serveiLlista.consultarLlista(idLlista);
-        Item res2 = serveiItem.consultarItem(idItem);
-        if (res == null || res2 == null) return ResponseEntity.notFound().build();
-        else {
-            if (res2.getIdLlista() != idLlista) return ResponseEntity.notFound().build();
-            else {
-                Item res3 = serveiItem.modificarItem(idItem);
-                return ResponseEntity.ok(res3);}
-        }
-    }
-
-    @DeleteMapping("/todolists/{idLlista}/todoitems/{idItem}")
-    public ResponseEntity<?> eliminarItem(@PathVariable int idLlista,@PathVariable int idItem) {
-        Llista res = serveiLlista.consultarLlista(idLlista);
-        if (res == null) return ResponseEntity.notFound().build();
-        else {
-            Item res2 = serveiItem.eliminarItem(idItem);
-            if (res2 == null) return ResponseEntity.notFound().build();
-            else return ResponseEntity.noContent().build();
-        }
-    }
-
-    //ITEMS
-
-    @GetMapping("/todoitems")
-    public ResponseEntity<?> llistarAllItems() {
-        if (serveiItem.llistarItems() == null) return ResponseEntity.notFound().build();
-        else return ResponseEntity.ok(serveiItem.llistarItems());
-    }
-
     //USUARIS
 
     @GetMapping("/todousers")
@@ -132,5 +31,145 @@ public class ControladorTaskt {
         Usuari res = serveiUsuari.afegirUsuari(nou);
         return new ResponseEntity<Usuari>(res, HttpStatus.CREATED);
     }
-    //
+
+    @GetMapping("/todousers/{idUsuari}")
+    public ResponseEntity<?> consultarUsuari(@PathVariable int idUsuari) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        if (res == null) return ResponseEntity.notFound().build();
+        else return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping("/todousers/{idUsuari}")
+    public ResponseEntity<?> eliminarUsuari(@PathVariable int idUsuari) {
+        Usuari res = serveiUsuari.eliminarUsuari(idUsuari);
+        if (res == null) return ResponseEntity.notFound().build();
+        else return ResponseEntity.noContent().build();
+    }
+
+    //LLISTES
+
+    @GetMapping("/todousers/{idUsuari}/todolists")
+    public ResponseEntity<?> llistarLlista(@PathVariable int idUsuari) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        if (res == null) return ResponseEntity.notFound().build();
+        else{
+            return ResponseEntity.ok(res.getLlistaLlista());
+        }
+    }
+
+    @GetMapping("/todousers/{idUsuari}/todolists/{idLlista}")
+    public ResponseEntity<?> consultarLlista(@PathVariable int idUsuari, @PathVariable int idLlista) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        Llista res2 = serveiLlista.consultarLlista(idLlista);
+        if (res == null) return ResponseEntity.notFound().build();
+        else{
+            if (res2 == null) return ResponseEntity.notFound().build();
+            else return ResponseEntity.ok(res2);
+        }
+    }
+
+    @PostMapping("/todousers/{idUsuari}/todolists")
+    public ResponseEntity<?> crearLlista(@PathVariable int idUsuari, @RequestBody Llista nou){
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        if (res == null) return ResponseEntity.notFound().build();
+        else {
+            if (nou.getIdUsuari() != idUsuari) return ResponseEntity.notFound().build();
+            else {
+                Llista res2 = serveiLlista.afegirLlista(nou);
+                return new ResponseEntity<Llista>(res2, HttpStatus.CREATED);
+            }
+        }
+    }
+
+    @DeleteMapping("/todousers/{idUsuari}/todolists/{idLlista}")
+    public ResponseEntity<?> eliminarLlista(@PathVariable int idUsuari,@PathVariable int idLlista) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        if (res == null) return ResponseEntity.notFound().build();
+        else {
+            Llista res2 = serveiLlista.eliminarLlista(idLlista);
+            if (res2 == null) return ResponseEntity.notFound().build();
+            else return ResponseEntity.noContent().build();
+        }
+    }
+
+    //@PutMapping("/todolists")
+    //public ResponseEntity<?> modificarLlista(@RequestBody Llista mod) {
+    //    Llista res = serveiLlista.modificarLlista(mod);
+    //    if (res == null) return ResponseEntity.notFound().build();
+    //    else return ResponseEntity.ok(res);
+    //}
+
+    @GetMapping("/todousers/{idUsuari}/todolists/{idLlista}/todoitems")
+    public ResponseEntity<?> llistarItemsLlista(@PathVariable int idUsuari, @PathVariable int idLlista) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        Llista res2 = serveiLlista.consultarLlista(idLlista);
+        if (res == null) return ResponseEntity.notFound().build();
+        else{
+            if (res2 == null) return ResponseEntity.notFound().build();
+            else {
+                return ResponseEntity.ok(res2.getLlistaItem());
+            }
+        }
+    }
+
+    @GetMapping("/todousers/{idUsuari}/todolists/{idLlista}/todoitems/{idItem}")
+    public ResponseEntity<?> consultarItemLlista(@PathVariable int idUsuari,@PathVariable int idLlista, @PathVariable int idItem) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        Llista res2 = serveiLlista.consultarLlista(idLlista);
+        Item res3 = serveiItem.consultarItem(idItem);
+        if (res == null) return ResponseEntity.notFound().build();
+        else{
+            if (res2 == null) return ResponseEntity.notFound().build();
+            else {
+                if (res3 == null) return ResponseEntity.notFound().build();
+                return ResponseEntity.ok(res3);
+            }
+        }
+    }
+
+    //si es pot crear es retorna CREATED
+    @PostMapping("/todousers/{idUsuari}/todolists/{idLlista}/todoitems")
+    public ResponseEntity<?> crearItem(@PathVariable int idUsuari, @PathVariable int idLlista, @RequestBody Item nou){
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        Llista res2 = serveiLlista.consultarLlista(idLlista);
+        if (res == null) return ResponseEntity.notFound().build();
+        else {
+            if (res2 == null) return ResponseEntity.notFound().build();
+            else {
+                if (nou.getIdLlista() != idLlista) return ResponseEntity.notFound().build();
+                else {
+                    Item res3 = serveiItem.afegirItem(nou);
+                    return new ResponseEntity<Item>(res3, HttpStatus.CREATED);
+                }
+            }
+        }
+    }
+
+    @PutMapping("/todousers/{idUsuari}/todolists/{idLlista}/todoitems/{idItem}")
+    public ResponseEntity<?> modificarItem(@PathVariable int idUsuari,@PathVariable int idLlista,@PathVariable int idItem) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        Llista res2 = serveiLlista.consultarLlista(idLlista);
+        Item res3 = serveiItem.consultarItem(idItem);
+        if (res == null || res2 == null || res3 == null) return ResponseEntity.notFound().build();
+        else {
+            if (res3.getIdLlista() != idLlista) return ResponseEntity.notFound().build();
+            else {
+                Item res4 = serveiItem.modificarItem(idItem);
+                return ResponseEntity.ok(res4);}
+        }
+    }
+
+    @DeleteMapping("/todousers/{idUsuari}/todolists/{idLlista}/todoitems/{idItem}")
+    public ResponseEntity<?> eliminarItem(@PathVariable int idUsuari, @PathVariable int idLlista,@PathVariable int idItem) {
+        Usuari res = serveiUsuari.consultarUsuari(idUsuari);
+        Llista res2 = serveiLlista.consultarLlista(idLlista);
+        if (res == null || res2 == null) return ResponseEntity.notFound().build();
+        else {
+            Item res3 = serveiItem.eliminarItem(idItem);
+            if (res3 == null) return ResponseEntity.notFound().build();
+            else return ResponseEntity.noContent().build();
+        }
+    }
+
+
 }
